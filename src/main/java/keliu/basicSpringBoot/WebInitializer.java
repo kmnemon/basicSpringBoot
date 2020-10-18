7da28d651108769f1057d9fc7af833e63364aa17
strickland2019 <koenliu@outlook.com>
2020-10-18 10:52:13 +0800

    2

---
+++ src/main/java/keliu/basicSpringBoot/WebInitializer.java
@@ -0,0 +1,27 @@
+package keliu.basicSpringBoot;
+
+import keliu.basicSpringBoot.config.DataConfig;
+import keliu.basicSpringBoot.config.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{
+                DataConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
